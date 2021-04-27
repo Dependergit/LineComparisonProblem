3cab72180025123fe978f3fd011aa7777ebb102a
Your Name <you@example.com>
2021-04-28 01:18:26 +0530

    created two lines

--- LineComparisonProblem.java
+++ LineComparisonProblem.java
@@ -5,5 +5,25 @@ public class LineComparisonProblem
     public static void main(String args[])
     {
         System.out.println("Welcome to line comprasion problem");
+
+        double x1, y1, x2, y2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the cordinates for first line");
+        x1=sc.nextDouble();
+        y1=sc.nextDouble();
+        x2=sc.nextDouble();
+        y2=sc.nextDouble();
+        double length1 = (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);
+        System.out.println(length1);
+
+
+        System.out.println("enter the cordinates for second line");
+        x1=sc.nextDouble();
+        y1=sc.nextDouble();
+        x2=sc.nextDouble();
+        y2=sc.nextDouble();
+        double length2 = (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);
+        System.out.println(length2);
+
     }
 }
